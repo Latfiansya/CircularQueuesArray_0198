0a61e1adfd90392df167483f7ced6742f2b5ab36
Latfiansya <latfiansyadzikri@gmail.com>
2024-05-30 11:41:34 +0700

    iterasi dari awal array hinga rear

--- CircularQueuesArray_0198/CircularQueuesArray_0198.cpp
+++ CircularQueuesArray_0198/CircularQueuesArray_0198.cpp
@@ -99,5 +99,13 @@ public:
                 }
 
                 FRONT_Position = 0;
+
+                //iterasi dari awal array hinga rear
+                while (FRONT_Position <= REAR_Position)
+                {
+                    cout << queue_array[FRONT_Position] << "  " <<
+                        FRONT_Position++;
+                }
+                cout << endl;
         }
 };
